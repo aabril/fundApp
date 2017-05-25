669f6c9d8172db46e6e955ab4dc1417eb2da4395
Albert Abril <albert.abril@gmail.com>
2017-05-25 18:57:18 +0200

    Adding Container and Content to look like Invision

---
+++ components/Container.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="Container">
+    <div class="Content">
+      <LemoorApp/>
+    </div>
+  </div>
+</template>
+
+<script>
+import LemoorApp from '~components/LemoorApp.vue'
+
+export default {
+  components: { LemoorApp }
+}
+</script>
+
+<style>
+.Container {
+  width: 416px;
+  height: 850px;
+  margin: 60px auto;
+  background: url('~assets/iphone6-portrait.png') no-repeat;
+}
+
+.Content {
+  position: absolute;
+  margin-top: 85px;
+  margin-left: 20px;
+  width: 375px;
+  height: 667px;
+  background: white;
+  cursor: url('~assets/mobile-cursor.png')  39 39, auto;
+}
+
+</style>
